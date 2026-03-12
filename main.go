45ec7d8e8e965bb7ec6363f567845d7366a096c0
archefnx <artykbaiev0000@gmail.com>
2026-03-12 15:08:48 +0500

    min-heap

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] > h[j] // min heap
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+
+	x := old[n-1]
+	*h = old[:n-1]
+
+	return x
+}
+
+func main() {
+	h := &IntHeap{5, 2, 9, 33, 3, 1}
+
+	heap.Init(h)
+
+	heap.Push(h, 321)
+
+	fmt.Println(heap.Pop(h)) // 1
+	fmt.Println(*h)          // 1
+}
